8aba75865d763169169e2ee33de924b94db050b3
Julia <70132973+j-bxco@users.noreply.github.com>
2026-07-20 22:16:35 +0800

    installed & setup vitest + supertest

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,8 @@
-import express from 'express';
 import { Server } from 'http';
 import { connectDB, disconnectDB } from './config/db.js';
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import authRoutes from './routes/authRoutes.js';
-import movieRoutes from './routes/movieRoutes.js';
-import watchlistRoutes from './routes/watchlistRoutes.js';
-import { globalLimiter } from './middleware/rateLimitMiddleware.js';
+import app from './app.js';
 
 connectDB();
-
-const app = express();
-
-// Body parsing middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(globalLimiter);
-
-// API Routes
-app.use('/auth', authRoutes);
-app.use('/movies', movieRoutes);
-app.use('/watchlist', watchlistRoutes);
-
-// Error handling middlewares
-app.use(notFound);
-app.use(errorHandler);
 
 const PORT = 5001;
 
